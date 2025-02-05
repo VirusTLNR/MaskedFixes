db1a03b1eb41024cda79b2788ebd3556234bf74b
VirusTLNR <king1985uk@hotmail.com>
2025-02-05 01:43:12 +0000

    improved upon invisibility fix so when masked kill players and players wear masks and die, there is no graphical glitch for a few frames

--- Patches/MaskedPlayerEnemyPatch.cs
+++ Patches/MaskedPlayerEnemyPatch.cs
@@ -1,10 +1,16 @@
 using HarmonyLib;
+using UnityEngine;
 
 namespace MaskedFixes.Patches
 {
     [HarmonyPatch(typeof(MaskedPlayerEnemy))]
     public class MaskedPlayerEnemyPatch
     {
+
+        static string oldDebugLine = "";
+        static GameObject tragedyRagdoll = StartOfRound.Instance.playerRagdolls[4]; //ragdoll when killed by Tragedy Mask/Masked.
+        static GameObject comedyRagdoll = StartOfRound.Instance.playerRagdolls[5]; //ragdoll when killed by Comedy Mask/Masked.
+
         [HarmonyPatch("SetVisibilityOfMaskedEnemy")]
         [HarmonyPrefix]
         //original implementation of MaskedPlayerEnemy.SetVisibilityOfMaskedEnemy with minor edits to prevent masked going invisible when players die.
@@ -14,10 +20,19 @@ namespace MaskedFixes.Patches
             {
                 return true; //someone else is fixing this fix so return true
             }
-            /*Plugin.Logger.LogError("allowNoPlayer?=" + __instance.allowSpawningWithoutPlayer);
-            Plugin.Logger.LogWarning("mimickedPlayer= " + (__instance.mimickingPlayer == null ? "null" : __instance.mimickingPlayer));
-            Plugin.Logger.LogWarning("mimickedPlayer.deadBody= " + ((__instance.mimickingPlayer == null || __instance.mimickingPlayer.deadBody == null) ? "null" : __instance.mimickingPlayer.deadBody));
-            Plugin.Logger.LogWarning("mimickedPlayer.deadBody.deactivated= " + (__instance.mimickingPlayer == null || __instance.mimickingPlayer.deadBody == null || __instance.mimickingPlayer.deadBody.deactivated == null ? "null" : __instance.mimickingPlayer.deadBody.deactivated));*/
+            //Plugin.Logger.LogError("tragedy = " + tragedyRagdoll.name); //just for checking i have the right ragdolls.
+            //Plugin.Logger.LogError("comedy = " + comedyRagdoll.name); //just for checking i have the right ragdolls.
+            string debugLine = 
+                "allowNoPlayer ?= " + __instance.allowSpawningWithoutPlayer +
+                " | mimickedPlayer = " + (__instance.mimickingPlayer == null ? "null" : __instance.mimickingPlayer) +
+                " | mimickedPlayer.IsPlayerDead = " + (__instance.mimickingPlayer == null ? "null" : __instance.mimickingPlayer.isPlayerDead) +
+                " | mimickedPlayer.deadBody= " + ((__instance.mimickingPlayer == null || __instance.mimickingPlayer.deadBody == null) ? "null" : __instance.mimickingPlayer.deadBody.name) + 
+                " | mimickedPlayer.deadBody.deactivated= " + (__instance.mimickingPlayer == null || __instance.mimickingPlayer.deadBody == null || __instance.mimickingPlayer.deadBody.deactivated == null ? "null" : __instance.mimickingPlayer.deadBody.deactivated);
+            /*if (oldDebugLine != debugLine)
+            {
+                oldDebugLine = debugLine; //to prevent log spam
+                Plugin.Logger.LogError(debugLine);
+            }*/
             if (__instance.allowSpawningWithoutPlayer)
             {
                 if (__instance.mimickingPlayer != null && __instance.mimickingPlayer.deadBody != null && !__instance.mimickingPlayer.deadBody.deactivated) //seems to be correct for non mimicked masked.
@@ -26,6 +41,7 @@ namespace MaskedFixes.Patches
                     {
                         __instance.enemyEnabled = false;
                         __instance.EnableEnemyMesh(false, false);
+                        Plugin.Logger.LogDebug(debugLine);
                         Plugin.Logger.LogDebug("(InvisFix)NonMimickedMasked-MaskedMadeInvisible");
                         return false;
                     }
@@ -34,16 +50,18 @@ namespace MaskedFixes.Patches
                 {
                     __instance.enemyEnabled = true;
                     __instance.EnableEnemyMesh(true, false);
+                    Plugin.Logger.LogDebug(debugLine);
                     Plugin.Logger.LogDebug("(InvisFix)NonMimickedMasked-MaskedMadeVisible");
                 }
                 return false;
             }
-            if (__instance.mimickingPlayer == null /*|| (__instance.mimickingPlayer.deadBody != null && !__instance.mimickingPlayer.deadBody.deactivated)*/) //this is now correct as masked should not go invisible when a player is dead, only should be invisible if there should be a player mimicked and there isnt.
+            if (__instance.mimickingPlayer == null || ((__instance.mimickingPlayer.deadBody == tragedyRagdoll || __instance.mimickingPlayer.deadBody == comedyRagdoll) && !__instance.mimickingPlayer.deadBody.deactivated)) //changed to prevent a graphical glitch for a few frames when a mask/masked kill a player.
             {
                 if (__instance.enemyEnabled)
                 {
                     __instance.enemyEnabled = false;
                     __instance.EnableEnemyMesh(false, false);
+                    Plugin.Logger.LogDebug(debugLine);
                     Plugin.Logger.LogDebug("(InvisFix)MimickedMasked-MaskedMadeInvisible");
                 }
                 return false;
@@ -52,6 +70,7 @@ namespace MaskedFixes.Patches
             {
                 __instance.enemyEnabled = true;
                 __instance.EnableEnemyMesh(true, false);
+                Plugin.Logger.LogDebug(debugLine);
                 Plugin.Logger.LogDebug("(InvisFix)MimickedMasked-MaskedMadeVisible");
             }
             return false;
